a53120c568478a3935c4fabd67095f1215d34ce9
ZedDevStuff <kouamejuniorauguste@gmail.com>
2023-05-23 13:13:08 +0000

    Ajoutez des fichiers projet.

---
+++ Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ZedUtil;
+public static class Extensions
+{
+    public static void ForEach<T>(this IEnumerable<T> i, Action<T> action)
+    {
+        foreach(T item in i)
+        {
+            action(item);
+        }
+    }
+    public static void TryAdd<T>(this List<T> i, T item, Func<T, bool> function)
+    {
+        if(function(item)) i.Add(item);
+    }
+}
