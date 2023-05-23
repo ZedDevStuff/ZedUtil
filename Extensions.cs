63df1a8d1669b277a35e7ecb00652808e94d774d
ZedDevStuff <kouamejuniorauguste@gmail.com>
2023-05-23 13:28:23 +0000

    Added basic summary

--- Extensions.cs
+++ Extensions.cs
@@ -1,6 +1,9 @@
 ﻿namespace ZedUtil;
 public static class Extensions
 {
+    /// <summary>
+    /// Executes an action using each item of the collection
+    /// </summary>
     public static void ForEach<T>(this IEnumerable<T> i, Action<T> action)
     {
         foreach(T item in i)
@@ -8,6 +11,9 @@ public static class Extensions
             action(item);
         }
     }
+    /// <summary>
+    /// Executes a function using the input <c>item</c>. If true, adds the item to the collection
+    /// </summary>
     public static void TryAdd<T>(this List<T> i, T item, Func<T, bool> function)
     {
         if(function(item)) i.Add(item);
